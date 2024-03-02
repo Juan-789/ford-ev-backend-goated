4fd95b300ff6e25c06d5f86320960929fc7a2aae
abeeh <adambeehler@cmail.carleton.ca>
2024-03-02 15:12:33 -0500

    Added three functions that handle checking tire pressure and a message of tire status. Two new variables, one for current pressure and for current car.

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 import requests
 token = "pk.eyJ1IjoianVhbi03ODkiLCJhIjoiY2x0YTQweHptMHAyYzJqcDlwZXgxMmswcSJ9.AhK_cgF4NpDOCBfyOd8hvw"
-def getting_route():
+
+# Example usage:
+current_pressure = 35  # current tire pressure in psi
+current_car = "etrongt"  # current car model
+
+def getting_route(start_place: str, end_place: str):
     services = ["directions"]
     service = services[0]  # can be changed to wtv we need
     type_transportation = "driving" #wtv type we need
 
-    start_longitude = -84.518641
-    start_latitude = 39.134270
-    end_longitude = -84.512023
-    end_latitude = 39.102779
+    start_longitude,  start_latitude= start_place.split(",")
 
-    starting_location = (start_longitude, start_latitude)
-    end_location = (end_longitude, end_latitude)
+
+    end_longitude, end_latitude = end_place.split(",")
+
     response = requests.get(f"https://api.mapbox.com/directions/v5/mapbox/{type_transportation}/"
                             f"{start_longitude},{start_latitude};{end_longitude},{end_latitude}?geometries=geojson&access_token={token}")
-    url = (f"https://api.mapbox.com/directions/v5/mapbox/{type_transportation}/"
-                            f"{start_longitude},{start_latitude};{end_longitude},{end_latitude}?geometries=geojson&access_token={token}")
-    print(response)
+    return response.url
 
-def addressToCoord(address: str):
+def addressToCoord(address: str)->str:
     addResponse = requests.get(f"https://api.mapbox.com/search/geocode/v6/forward?q={address}&access_token={token}")
+    # print(addResponse.url)    check url
     data = addResponse.json()
     longitude, latitude = data.get("features")[0].get("geometry").get("coordinates")
-    print(longitude)
-    print(latitude)
-    print(addResponse.url)
-addressToCoord("111 wllington Ottawa ")
+    return str(longitude)+ ","+str(latitude) # coords
+
+beginning = addressToCoord("111 wellington Ottawa ")
+end = addressToCoord("1015 bank street ottawa")
+print(getting_route(beginning, end))
+
+def get_recommended_psi(car_model):
+    recommended_psi_dict = {
+        "mme": 39,
+        "lightning": 37,
+        "nautilus": 35,
+        "etrongt": 42
+    }
+    return recommended_psi_dict.get(car_model, 0)  # Return 0 if car_model not found
+
+def tire_pressure_status(current_psi, car_model):
+    recommended_psi = get_recommended_psi(car_model)
+    if current_psi >= recommended_psi - 5 and current_psi <= recommended_psi + 2:
+        return 0
+    elif current_psi > recommended_psi + 2:
+        return 2
+    else:
+        return 1
+
+def get_status_message(status):
+    if status == 0:
+        return "Tire pressure is within a safe range"
+    elif status == 2:
+        return "Tire pressure is higher, you may or may not want to lower it"
+    else:
+        return "Tire pressure is significantly below recommendation, it's suggested that you increase it"
+
+tire_status = tire_pressure_status(current_pressure, current_car)
+tire_message = get_status_message(tire_status)
+print("Tire pressure status:", tire_message)
