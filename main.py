d6ca9ec288cabfa898f09cb0a94c47b24efe1e05
abeeh <adambeehler@cmail.carleton.ca>
2024-03-02 17:03:17 -0500

    Added two functions that return the battery usage data for a trip and another that returns the car's energy utilization as a percentage.

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
+import random
 token = "pk.eyJ1IjoianVhbi03ODkiLCJhIjoiY2x0YTQweHptMHAyYzJqcDlwZXgxMmswcSJ9.AhK_cgF4NpDOCBfyOd8hvw"
 
 # Example usage:
-current_pressure = 35  # current tire pressure in psi
+current_pressure = 39  # current tire pressure in psi
 current_car = "etrongt"  # current car model
+mode = "sport"  # Example drive mode
 
 def getting_route(start_place: str, end_place: str):
     services = ["directions"]
@@ -59,3 +61,26 @@ def get_status_message(status):
 tire_status = tire_pressure_status(current_pressure, current_car)
 tire_message = get_status_message(tire_status)
 print("Tire pressure status:", tire_message)
+
+# Randomly generate battery usage for a trip and return each aspects usage percentage
+def trip_energy():
+    climate_use = random.randint(5, 12)
+    route = random.randint(60, 75)
+    accessories = random.randint(5, 12)
+    exterior_temperature = 100 - (climate_use + route + accessories)
+    return climate_use, route, accessories, exterior_temperature
+
+climate_use, route, accessories, exterior_temperature = trip_energy()
+print("Climate Use:", climate_use, "- Route:", route, "- Accessories:", accessories, "- Exterior Temperature:", exterior_temperature)
+
+# Return the utilization usage percentage of the car's power
+def drive_mode(mode):
+    if mode == "eco":
+        return 60
+    elif mode == "sport":
+        return 100
+    else:
+        return 80  # default to normal drive mode
+
+energy_consumption = drive_mode(mode)
+print("Energy output usage for", mode, "mode:", energy_consumption, "%")
